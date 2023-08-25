b188a37287cc1454603946c06286006699c72f37
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2023-08-25 11:17:10 +0530

    First commit

---
+++ LambdaSum/Math.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace LambdaSum
+{
+    public class Data
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+    }
+
+    public class Math
+    {
+        public APIGatewayProxyResponse Add(Data data, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse()
+            {
+                Headers = new Dictionary<string, string>()
+                {
+                    { "Access-Control-Allow-Origin", "*" },
+                    { "Access-Control-Allow-Headers", "*" },
+                    { "Access-Control-Allow-Methods", "OPTIONS,POST" }
+                }
+            };
+
+            response.Body = "Sum = " + (data.First + data.Second);
+
+            return response;
+        }
+    }
+}
